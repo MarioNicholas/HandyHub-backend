cca05c26d854d9fcb3be616ddf794e78c28ed4dd
MarioNicholas <18221061@std.stei.itb.ac.id>
2024-05-27 15:27:33 +0700

    TEST DEPLOY

--- routes/admin.js
+++ routes/admin.js
@@ -8,7 +8,7 @@ const router = express.Router();
 
 router.post("/add-service", adminController.addService)
 router.delete("/delete-service/:serviceID",adminController.deleteService)
-router.patch*('/edit-service/:serviceID', adminController.editService)
+router.patch('/edit-service/:serviceID', adminController.editService)
 
 // /admin/add-product => GET
 // router.get('/add-product', adminController.getAddProduct);
