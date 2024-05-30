fac38b274f1e7b827b5fc4526849e4f7ad29b7b8
MarioNicholas <18221061@std.stei.itb.ac.id>
2024-05-30 21:09:12 +0700

    fix

--- controllers/admin.js
+++ controllers/admin.js
@@ -6,7 +6,7 @@ const Order = require("../models/order")
 const expressValidator = require("express-validator");
 const fs = require("fs");
 const path = require("path");
-const expressValidator = require("express-validator");
+// const expressValidator = require("express-validator");
 
 
 // const Product = require('../models/product');
